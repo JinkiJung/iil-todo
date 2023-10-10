3bc81d69527e77cd8b0ef1e17c9850000c73904d
Jinki <your.jinki.jung@gmail.com>
2023-10-10 09:57:06 +0200

    condition selection form

--- src/element/util/conditionSelector.tsx
+++ src/element/util/conditionSelector.tsx
@@ -1,9 +1,10 @@
 // ConditionSelector component that works like IilSelector but for conditions
 
 import React, { useEffect, useState } from "react";
-import { Form } from "react-bootstrap";
+import { Button, Form, InputGroup } from "react-bootstrap";
 import { AsyncTypeahead, Typeahead } from "react-bootstrap-typeahead";
-import { ConditionControllerApi, ConditionDto } from "../../ill-repo-client";
+import { ConditionControllerApi, ConditionDto, ConditionDtoTypeEnum } from "../../ill-repo-client";
+import * as Icon from 'react-bootstrap-icons';
 
 export interface ConditionSelectorProp {
     onConditionChange: ((chosenConditions: any[]) => void);
@@ -15,6 +16,7 @@ export const ConditionSelector = (
     { onConditionChange, inputRef, givenCondition }: ConditionSelectorProp
 ) => {
     const [conditions, setConditions] = useState<ConditionDto[]>([]);
+    const [condition, setCondition] = useState<ConditionDto | undefined>(givenCondition);
 
     const [singleSelection, setSingleSelection] = useState<ConditionDto[]>(givenCondition ? [givenCondition] : []);
     const [isLoading, setIsLoading] = useState(false);
@@ -32,37 +34,97 @@ export const ConditionSelector = (
 
     const handleSearch = (query: string) => {
         setIsLoading(true);
-
-        conditionControllerApi.getConditions().then((resp) => {
-            const { data } = resp;
-            setConditions(data);
-            setIsLoading(false);
-        });
+        // check the condition.type is one of a value in ConditionDtoTypeEnum
+        if (condition?.type && Object.values(ConditionDtoTypeEnum).includes(condition.type)) {
+            conditionControllerApi.getConditionsByType(condition.type).then((resp) => {
+                const { data } = resp;
+                setConditions(data);
+                setIsLoading(false);
+            });
+        }
     };
+
+    // a function returns Icon and ConditionType value corresponding to given ConditionType
+    // Icon.Clock for ConditionDtoTypeEnum.TIME
+    // Icon.CursorFill for ConditionDtoTypeEnum.LOCATION
+    // Icon.Braces for ConditionDtoTypeEnum.IIL_VARIABLE
+    // Icon.Files for ConditionDtoTypeEnum.IIL_INPUT
+    // Icon.ArrowRightCircle for ConditionDtoTypeEnum.IIL_STATE
+    // Icon.BoxArrowInDownLeft for ConditionDtoTypeEnum.EVENT
+    const getIconAndTypeString = (conditionType: ConditionDtoTypeEnum) => {
+        switch (conditionType) {
+            case ConditionDtoTypeEnum.TIME:
+                return <><Icon.Clock />{" "+ conditionType}</>;
+            case ConditionDtoTypeEnum.LOCATION:
+                return <><Icon.CursorFill />{" "+ conditionType}</>
+            case ConditionDtoTypeEnum.IILVARIABLE:
+                return <><Icon.Braces />{" "+ conditionType}</>
+            case ConditionDtoTypeEnum.IILINPUT:
+                return <><Icon.Files />{" "+ conditionType}</>
+            case ConditionDtoTypeEnum.IILSTATE:
+                return <><Icon.ArrowRightCircle />{" "+ conditionType}</>
+            case ConditionDtoTypeEnum.EVENT:
+                return <><Icon.BoxArrowInDownLeft />{" "+ conditionType}</>
+            default:
+                return <><Icon.Clock />{" "+ conditionType}</>
+        }
+    }
+
 
     return (
         <Form.Group>
-            <AsyncTypeahead
-                filterBy={filterBy}
-                id="async-example"
-                isLoading={isLoading}
-                labelKey="name"
-                minLength={3}
-                ref={inputRef}
-                onSearch={handleSearch}
-                onChange={(selected) => {
-                    onConditionChange!(selected);
-                    setSingleSelection(selected as ConditionDto[]);
-                }}
-                options={conditions}
-                placeholder="Search for a condition"
-                renderMenuItemChildren={(option: ConditionDto) => (
-                    <>
-                        <span>{option.name}</span>
-                    </>
-                )}
-                selected={singleSelection}
-            />
+            <InputGroup>
+                {
+                    condition?.type ?
+                        <Button
+                            variant="secondary"
+                            onClick={() => {
+                                setCondition!({ condition, type: undefined } as ConditionDto);
+                                inputRef.current && (inputRef.current as any).clear();
+                                setSingleSelection([]);
+                            }}
+                        >
+                            {getIconAndTypeString(condition.type)}
+                        </Button>
+                        :
+                        // create buttons with value and name of all ConditionDtoTypeEnum
+                        Object.values(ConditionDtoTypeEnum).map((conditionType) => {
+                            return (
+                                <Button
+                                    key={conditionType}
+                                    variant="outline-secondary"
+                                    onClick={() => {
+                                        setCondition!({ condition, type: conditionType } as ConditionDto);
+                                    }}
+                                >
+                                    {getIconAndTypeString(conditionType)}
+                                </Button>
+                            );
+                        })
+                }
+                <AsyncTypeahead
+                    filterBy={filterBy}
+                    id="async-example"
+                    isLoading={isLoading}
+                    labelKey="name"
+                    className="flex-grow-1"
+                    minLength={1}
+                    ref={inputRef}
+                    onSearch={handleSearch}
+                    onChange={(selected) => {
+                        onConditionChange!(selected);
+                        setSingleSelection(selected as ConditionDto[]);
+                    }}
+                    options={conditions}
+                    placeholder="Search for a condition"
+                    renderMenuItemChildren={(option: ConditionDto) => (
+                        <>
+                            <span>{option.name}</span>
+                        </>
+                    )}
+                    selected={singleSelection}
+                />
+            </InputGroup>
         </Form.Group>
     );
 }
